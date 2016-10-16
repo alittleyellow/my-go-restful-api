a972889181e6a39b9a2de14266a61386e11f8c5e
huangyang <hy303924478@gmail.com>
2016-10-16 22:17:39 +0800

    update code

--- src/router/router.go
+++ src/router/router.go
@@ -2,8 +2,9 @@ package router
 
 import (
 	"net/http"
-	"fmt"
 	"regexp"
+	"encoding/json"
+	"fmt"
 )
 
 type Router struct {
@@ -14,12 +15,24 @@ func NewRouter() *Router {
 	return &Router{}
 }
 
-func (this *Router) Handler(resp http.ResponseWriter, req *http.Request)  {
+func (this *Router) Handler(resp http.ResponseWriter, req *http.Request) {
 	match, _ := regexp.MatchString("/favicon.ico", req.URL.Path)
 	if match == true {
-		
+
 	} else {
-		this.Url = req.URL.Path
-	}	
-	fmt.Println(this.Url)
+		Url = req.URL.Path
+
+		w, err := this.Url();
+		if err == nil {
+			fmt.Fprintf(resp, string(w))
+		}
+		
+	}
+}
+
+func (this *Router) GetJson() ([]byte, error) {
+	pass := make(map[string]string)
+	pass["name"] = "huangyang"
+	pass["age"] = "18"
+	return json.MarshalIndent(pass, "", "")
 }
